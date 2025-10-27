ba45dacedcc7f9be42ab6ceb7b2e526279899f4e
dkpapp <118980727+dkpapp@users.noreply.github.com>
2025-10-27 11:57:09 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__, static_folder='.', static_url_path='')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
